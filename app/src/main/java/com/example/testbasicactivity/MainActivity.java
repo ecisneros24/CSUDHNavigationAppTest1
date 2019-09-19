5825b68c7364289a963f0c9ad09521300b1619ee
ecisneros24 <elthoncisneros01@gmail,.com>
2019-09-18 19:11:55 -0700

    Version3 test

--- app/src/main/java/com/example/testbasicactivity/MainActivity.java
+++ app/src/main/java/com/example/testbasicactivity/MainActivity.java
@@ -13,7 +13,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-//text
+//test2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
