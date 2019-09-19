02cb76bbe9923dbe06f2b43e578f5ee05f5e5fd5
ceejaycee@cox.net <ceejaycee@cox.net>
2019-09-18 19:46:21 -0700

    Testing

--- app/src/main/java/com/example/testbasicactivity/MainActivity.java
+++ app/src/main/java/com/example/testbasicactivity/MainActivity.java
@@ -36,7 +36,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "Hello World");
+        Log.d("Test");
         Log.d("MainActivity", "Hi Back");
+
     }
 
     @Override
