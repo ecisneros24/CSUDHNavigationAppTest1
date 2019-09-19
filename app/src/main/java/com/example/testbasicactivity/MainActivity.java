3650643879aee831f950a85a1dad24a32de93aa7
ceejaycee@cox.net <ceejaycee@cox.net>
2019-09-18 19:40:33 -0700

    Testing

--- app/src/main/java/com/example/testbasicactivity/MainActivity.java
+++ app/src/main/java/com/example/testbasicactivity/MainActivity.java
@@ -32,10 +32,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
     }
 
     @Override
